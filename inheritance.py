bc67301a6d4178ee137c158c71ace4279236b273
Micah Gooden <micah734@me.com>
2022-01-11 11:23:36 -0600

    inheritance assignment

---
+++ inheritance.py
@@ -0,0 +1,27 @@
+
+
+class vehicle: #creating the class vehicle
+    brand="",
+    model="",
+    year=0,
+    steering method= ""
+
+
+#creating a child of vehicles
+
+class airplane(vehicle):
+    number of wings=0,
+    capacity=0
+
+
+#creating another child of vehicles
+
+class motorcycle(vehicle):
+    number of wheels=0,
+    engine=""
+    
+    
+    
+
+    
+    
