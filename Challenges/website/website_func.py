07648ce029cf2fb2cb57e01d8e1ed52ff8f9ac5f
Micah Gooden <micah734@me.com>
2022-01-21 19:26:26 -0500

    updated  website challenge

--- Challenges/website/website_func.py
+++ Challenges/website/website_func.py
@@ -6,21 +6,21 @@ import website_gui,website_main
 
 def build(self):
     self.g=open("website1.html","w")
-    self.g.write("<html><body><h1>{}</h1></body></html>".format(getBody))
+    self.g.write("<html><body><h1>{}</h1></body></html>".format(getBody(self)))
     self.g.close()
     
-    self.url='file:/Users/admin/Documents/GitHub/Python_Projects/Challenges/website/website1.html'
+    self.url='website1.html'
     webbrowser.open_new_tab(self.url)
 
 
 def getBody(self):
-    input=website_func.self.txt_body.get("1.0",END)
-    print(getBody)
+    userText=self.txt_body.get("1.0","end-1c")
+    return userText
       
 
     
 def clear(self):
-    self.txt_body.delete("1.0",END)
+    self.txt_body.delete("1.0","END")
 
 def ask_quit(self):
     if messagebox.askokcancel("Exit program", "Okay to exit application?"):
