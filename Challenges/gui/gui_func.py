938924ec9d769bdc88ddf6d342be60f24dea99c4
Micah Gooden <micah734@me.com>
2022-01-22 12:36:40 -0500

    fixed functions

--- Challenges/gui/gui_func.py
+++ Challenges/gui/gui_func.py
@@ -25,22 +25,22 @@ def callbackdst(self):
 
 
 def compare(self):
-    self.source1='{}'.format(path(self))
-    self.recieving='{}'.format(pathdst(self))
+    self.source1=self.txt_browse.get()
+    self.recieving1=self.txt_browse2.get()
     self.files=os.listdir(self.source1)
 
     self.seconds_in_day=24*60*60
 
     now=time.time()
-    before=now-seconds_in_day
+    before=now-self.seconds_in_day
 
     def last_mod_time(fname):
-        return os.path.getmtime(fname)
+        return self.source1_fname
 
-    for fname in files:
+    for fname in self.files:
         self.source1_fname=os.path.join(self.source1,fname)
-        if last_mod_time(self,source1_fname)>before:
-            self.recieving_fname=os.path.join(self.recieving,fname)
+        if last_mod_time(self.source1_fname)>before:
+            self.recieving_fname=os.path.join(self.recieving1,fname)
             shutil.move(self.source1_fname,self.recieving_fname)
 
 
