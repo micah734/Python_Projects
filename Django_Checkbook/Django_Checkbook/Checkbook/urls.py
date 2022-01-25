a710f0856ba445e2c41d0cd6f7ba6f6996ed4fe7
Micah Gooden <micah734@me.com>
2022-01-25 16:36:22 -0500

    Added Django Checkbook App

---
+++ Django_Checkbook/Django_Checkbook/Checkbook/urls.py
@@ -0,0 +1,12 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib import admin
+from django.conf.urls import include
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home,name='index'),
+    path('create/',views.create_account,name='create'),
+    path('<int:pk>balance/',views.balance,name='balance'),
+    path('transaction/',views.transaction,name='transaction')
+]
